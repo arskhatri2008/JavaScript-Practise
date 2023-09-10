6b52b9762bb563ff3a81a95c8991d35765d877ed
Arsalan Ahmed <arskhatri2008@gmail.com>
2023-09-10 07:53:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const year = 2024
+
+// if(year % 4 === 0) {
+//     console.log('The year ' , year , " is a leap year")
+// }else if(year % 100 !== 0) {
+//     console.log('The year ' , year , " is not a leap year")
+// }else if(year % 400 === 0){
+//     console.log('Thea year' , year , " is a leap year")
+// }
+
+// if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+//     console.log('The year ' , year , " is a leap year")
+// }
