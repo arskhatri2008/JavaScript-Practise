3fc578b317f1c648b5dffe84c074816a15da2bc4
Arsalan Ahmed <arskhatri2008@gmail.com>
2023-09-10 12:08:05 +0500

    practice

--- index.js
+++ index.js
@@ -11,3 +11,53 @@
 // if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
 //     console.log('The year ' , year , " is a leap year")
 // }
+
+
+// age = 19
+
+// if(age >= 18){
+//     console.log('You are eligible to vote')
+// }else{
+//     console.log('You are not eligible to vote')
+// }
+
+// console.log((age >= 18) ? 'You are eligible to vote' : 'You are not eligible to vote')
+
+// const area = 'circle'
+// const PI = 3.14, l=5, b=4, r=3
+
+// if(area === 'circle'){
+//     console.log(PI * r**2)
+// }else if(area === 'rectangle'){
+//     console.log(l * b)
+// }else if(area === 'triangle'){
+//     console.log(0.5 * l * b)
+// }else{
+//     console.log('Invalid area')
+// }
+
+// switch(area){
+//     case 'circle': console.log(PI * r**2)
+//     break
+//     case 'rectangle': console.log(l * b)
+//     break
+//     case  'triangle': console.log(0.5 * l * b)
+//     break
+//     default: console.log('Invalid area')
+
+// }
+
+// let num = 0
+
+// while(num <= 10){
+//     num++;
+//     console.log(num)
+// }
+
+// let num = 20;
+
+// do{
+//     num++
+//     console.log(num)
+// }while(num <= 10)
+
